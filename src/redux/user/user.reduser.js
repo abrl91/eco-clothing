a9ebe74ecf9ca47ad319512d13a6e438e083884a
Amit Barletz <amitbarletz@Amits-MBP.lan>
2020-03-23 16:15:19 +0200

    added redux and implemented user reduser and functions to header and app components

---
+++ src/redux/user/user.reduser.js
@@ -0,0 +1,19 @@
+// reduser - a function that gets 2 properties: 1) current state  2) action: { type: string, payload: any }
+const INITIAL_STATE = {
+    currentUser: null
+};
+
+// reduser gets all the actions, therefore in the switch we need default case
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
