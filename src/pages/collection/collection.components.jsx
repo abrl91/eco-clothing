179fe7dc394e61b5c5fc22f23654cd7e34b34248
Amit Barletz <amitbarletz@Amits-MBP.lan>
2020-03-31 10:37:05 +0300

    move directory and shop data to reducer and created respective selectors

--- src/pages/collection/collection.components.jsx
+++ src/pages/collection/collection.components.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-// import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import './collection.style.scss';
 import { connect } from 'react-redux';
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
-const CollectionPage = ({ collection }) => (
+const CollectionPage = ({ collection }) => {
+        const { title, items } = collection;
+    return (
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            { items.map(item => (<CollectionItem key={item.id} item={item} />)) }
+        </div>
     </div>
 );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
